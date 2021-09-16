e69c65ffc62b885c42b00073b5a18005d527bbf9
chaos-soft <mail@57st.net>
2021-09-16 19:54:41 +0300

    Чистка игр

--- games/far cry 5.sh
+++
@@ -1,3 +0,0 @@
-STEAM_COMPAT_DATA_PATH='/mnt/polina/games/far cry 5' \
-steam-run ./proton run '/mnt/polina/games/far cry 5/pfx/drive_c/Program Files (x86)/Ubisoft/Ubisoft Game Launcher/games/Far Cry 5/bin/FarCry5.exe'
-# steam-run ./proton run '/mnt/polina/games/far cry 5/pfx/drive_c/Program Files (x86)/Ubisoft/Ubisoft Game Launcher/UbisoftConnect.exe'
