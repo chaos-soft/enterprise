3adf9b3ea25fab05fc0f4c11ad643a81e3a841ae
chaos-soft <mail@57st.net>
2024-06-02 17:25:08 +0300

    Caja и Task Manager из Xfce

--- void/post install.sh
+++ void/post install.sh
@@ -3,6 +3,7 @@ rm -r ~/.local/share/fonts
 cp -rp ~/Documents/fonts ~/.local/share
 install -pDvm644 /etc/sway/config ~/Documents/python/tools/backup/home__chaos__.config__sway__config
 install -pDvm644 /usr/share/doc/mangohud/MangoHud.conf.example ~/Documents/python/tools/backup/home__chaos__tmp__mh
+install -pDvm644 /usr/share/vulkan/implicit_layer.d/MangoHud.x86_64.json ~/Documents/python/tools/backup/usr__share__vulkan__implicit_layer.d__MangoHud.x86.json
 install -pDvm644 ~/Documents/enterprise/fonts.conf ~/.config/fontconfig/fonts.conf
 install -pDvm644 ~/Documents/enterprise/gnome/gnome-shell.css ~/.themes/xxx/gnome-shell/gnome-shell.css
 install -pDvm644 ~/Documents/enterprise/gnome/gtk.css ~/.config/gtk-3.0/gtk.css
@@ -22,7 +23,7 @@ sudo ln -s /etc/sv/docker /var/service/
 sudo ln -s /etc/sv/gdm /var/service/
 sudo ln -s /etc/sv/zramen /var/service/
 
-sudo usermod -aG docker $USER
+sudo usermod -aG docker,gamemode $USER
 
 curl -LJ -o ~/Documents/python/tools/backup/home__chaos__.config__gtk-4.0__gtk.css https://raw.githubusercontent.com/lassekongo83/adw-colors/main/themes/nord/gtk.css
 curl -LJ -o ~/Documents/python/tools/backup/home__chaos__tmp__vkBasalt.conf https://raw.githubusercontent.com/DadSchoorse/vkBasalt/master/config/vkBasalt.conf
