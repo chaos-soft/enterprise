d56ab01da8be018feff73dab4fd1a13b6b161031
chaos-soft <mail@57st.su>
2024-11-22 22:33:03 +0300

    v20241122

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,5 @@
 subl4 ~/Documents/python/miranda/config.ini ~/Documents/python/velvet/store/html/articles/57.html
 read -n 1 -p 'Waiting...'
 scp -pr ~/Documents/python/miranda/config.ini polina:/root/python/miranda/
+scp -pr ~/Documents/python/miranda/twitch.json polina:/root/python/miranda/
 scp -pr ~/Documents/python/velvet/store/html/articles/57.html polina:/root/python/velvet/store/html/articles/
