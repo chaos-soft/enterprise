f9638b34258824cd5df3e911c36c1ab63d10a1a7
chaos-soft <mail@57st.su>
2025-04-10 18:13:19 +0300

    Удаление NixOS

--- nixos/bootloader.sh
+++
@@ -1,4 +0,0 @@
-sudo mount /dev/sda2 /mnt
-sudo mount /dev/sda1 /mnt/boot
-sudo nixos-enter
-# NIXOS_INSTALL_BOOTLOADER=1 /nix/var/nix/profiles/system/bin/switch-to-configuration boot
--- nixos/install.sh
+++
@@ -1,7 +0,0 @@
-sudo mount /dev/disk/by-label/alisa /mnt
-sudo mkdir -p /mnt/boot
-sudo mount /dev/disk/by-label/BOOT /mnt/boot
-sudo nixos-generate-config --root /mnt
-sudo nano /mnt/etc/nixos/configuration.nix
-echo 'nixos-install' && read -n 1 -s
-sudo nixos-install
